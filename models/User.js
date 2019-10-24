d812a7f4d04582b3702915caa6415d89f85c34b5
Daniel Visca <danielvisca96@gmail.com>
2019-10-24 18:18:16 -0400

    Made users model a simple list

--- models/User.js
+++ models/User.js
@@ -1,26 +1,38 @@
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
-const model_name = "users";
+//const model_name = "users";
+const users = []
 
-const schmea = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    index: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date_created: {
-    type: Date,
-    default: Date.now()
-  }
-});
+function createUser(name, email, password) {
+  let user = {
+    "name":name,
+    "email":email,
+    "password":password
+  };
+  users.push(user)
+}
 
-module.exports = mongoose.model(model_name, schmea, model_name);
+// Create schema for users
+// const schmea = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     index: true,
+//     unique: true
+//   },
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   date_created: {
+//     type: Date,
+//     default: Date.now()
+//   }
+// });
+
+// module.exports = mongoose.model(model_name, schmea, model_name);
+module.exports = createUser();
