2b342988bf12cd7254c51ecbf0ce0ac2d96a65d2
shlok <shloksomani@ymail.com>
2019-10-24 16:54:40 -0400

    Added contents to readme for installations and basic things

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+
+const user = require("../models/User");
+
+router.get("/login", function(req, res) {
+	const data = {};
+
+	data.title = "Login";
+	data.errors = req.flash("error");
+	data.user = req.user;
+
+	res.render("auth/login", data);
+});
+
+router.get("/signup", function(req, res) {
+	const data = {};
+
+	data.title = "Signup";
+	data.errors = req.flash("error");
+	data.user = req.user;
+
+	res.render("auth/signup", data);
+});
