9e5e3cdbc049f3970aef0686b28bf39932001fb6
= <danielvisca96@gmail.com>
2019-11-03 15:09:31 -0500

    Search works, randomized article orders, css broke

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ const User = require("../models/User");
 let parsed_data;
 
 
-// Get the dummydata from JSON file
+// Get the dummy_data from JSON file
 try {
-	const data = fs.readFileSync('./public/dummydata.json');
+	const data = fs.readFileSync('./public/dummy_data.json');
 	parsed_data = JSON.parse(data);
 	console.log("BLAST OFF!")
 } catch (e) {
@@ -16,13 +16,37 @@ try {
 	console.log("Houston we have a problem")
 }
 
+function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
 // GET home page.
 router.get("/", function (req, res, next) {
 	const data = {};
 	// data.title = req.user.name;
 	data.user = req.user;
-	data.dummy_data = parsed_data;
-	console.log(data.dummy_data)
+	//data.dummy_data = parsed_data;
+	data.dummy_data = []
+	// randomize the order
+
+	// for each bias
+	for (let bias in parsed_data) {
+		// List of articles that have the bias
+		const bias_list = parsed_data[bias]
+		for (let i = 0; i < 2; i++) {
+			// each article in the article list
+			const article = bias_list[i]
+			console.log(article.title);
+
+			data.dummy_data.push(article)
+		}
+	}
+	shuffleArray(data.dummy_data)
 	res.render("index", data);
 });
 
@@ -40,7 +64,8 @@ router.get("/dash", authMiddleware.loginRequired, function (req, res, next) {
 router.get("/left_bias", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.left_bias;
+	data.dummy_data = parsed_data.left_bias;
+	shuffleArray(data.dummy_data)
 	res.render("left_bias", data);
 })
 
@@ -48,7 +73,8 @@ router.get("/left_bias", function (req, res, next) {
 router.get("/left_center_bias", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.left_center_bias;
+	data.dummy_data = parsed_data.left_center_bias;
+	shuffleArray(data.dummy_data)
 	res.render("left_center_bias", data);
 })
 
@@ -56,7 +82,8 @@ router.get("/left_center_bias", function (req, res, next) {
 router.get("/least_bias", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.least_bias;
+	data.dummy_data = parsed_data.least_bias;
+	shuffleArray(data.dummy_data)
 	res.render("least_bias", data);
 })
 
@@ -64,7 +91,8 @@ router.get("/least_bias", function (req, res, next) {
 router.get("/right_center_bias", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.right_center_bias;
+	data.dummy_data = parsed_data.right_center_bias;
+	shuffleArray(data.dummy_data)
 	res.render("right_center_bias", data);
 })
 
@@ -72,7 +100,8 @@ router.get("/right_center_bias", function (req, res, next) {
 router.get("/right_bias", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.right_bias;
+	data.dummy_data = parsed_data.right_bias;
+	shuffleArray(data.dummy_data)
 	res.render("right_bias", data);
 })
 
@@ -80,7 +109,8 @@ router.get("/right_bias", function (req, res, next) {
 router.get("/pro_science", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.pro_science;
+	data.dummy_data = parsed_data.pro_science;
+	shuffleArray(data.dummy_data)
 	res.render("pro_science", data);
 })
 
@@ -88,7 +118,9 @@ router.get("/pro_science", function (req, res, next) {
 router.get("/questionable_sources", function (req, res, next) {
 	const data = {};
 	data.user = req.user;
-	data.dummydata = parsed_data.questionable_sources;
+	data.dummy_data = parsed_data.questionable_sources;
+	console.log(parsed_data)
+	shuffleArray(data.dummy_data)
 	res.render("questionable_sources", data);
 })
 
@@ -122,40 +154,70 @@ router.post("/admin", function (req, res, next) {
 
 router.get("/index/keywords", function(req, res, next){
 	// //const query = req
-	console.log("logging keywords");
+	console.log("KEYWORDSEARCH");
+	function random_dummy_data(){
+		data.dummy_data = []
+
+		// for each bias
+		for (let bias in parsed_data) {
+			// List of articles that have the bias
+			const bias_list = parsed_data[bias]
+			for (let i = 0; i < 2; i++) {
+				// each article in the article list
+				const article = bias_list[i]
+				console.log(article.title);
+
+				data.dummy_data.push(article)
+			}
+		}
+	}
 	//console.log(location);
 	data = {}
 	data.user = req.user;
 	// get the query from url
-	keywords_string = req.url.split('=')[1]
-	data.keywords = keywords_string.split("+").filter(function(el) {
-		return el != "";
-	})
-	// for strings that are not empty, search through keywords in dummy data. If keyword in it. Add to array
-	data.dummy_data = []
-	
-	// for each bias
-	for (let bias in parsed_data) {
-		// List of articles that have the bias
-		const bias_list = parsed_data[bias]
-		for (let i = 0; i < bias_list.length; i++){
-			// each article in the article list
-			const article = bias_list[i]
-			for (let j = 0; j < article.keywords.length; j++){
-				// each keyword attributed to the article
-				keyword = article.keywords[j]
-				// compare the users searched words to the articles keywords
-				for (let k = 0; k < data.keywords.length; k++){
-					search_word = data.keywords[k]
-					if (keyword == search_word ){
-						// if they match, add the article to dummy data
-						data.dummy_data.push(article)
+	if (req.url){
+		keywords_string = req.url.split('=')[1]
+		if (keywords_string != '') {
+			
+		
+			console.log("After the break")
+			data.keywords = keywords_string.split("+").filter(function(el) {
+				return el != "";
+			})
+			// for strings that are not empty, search through keywords in dummy data. If keyword in it. Add to array
+			data.dummy_data = []
+			
+			// for each bias
+			for (let bias in parsed_data) {
+				// List of articles that have the bias
+				const bias_list = parsed_data[bias]
+				for (let i = 0; i < bias_list.length; i++){
+					// each article in the article list
+					const article = bias_list[i]
+					for (let j = 0; j < article.keywords.length; j++){
+						// each keyword attributed to the article
+						keyword = article.keywords[j]
+						// compare the users searched words to the articles keywords
+						for (let k = 0; k < data.keywords.length; k++){
+							search_word = data.keywords[k]
+							if (keyword == search_word ){
+								// if they match, add the article to dummy data
+								data.dummy_data.push(article)
+							}
+						}
 					}
 				}
 			}
 		}
+		else {
+			random_dummy_data()
+		}
 	}
-	
+	else {
+		random_dummy_data()
+	}
+
+				
 	res.render("index", data);
 })
 
