e2c3a765d62a9db390815aa39d9b4746eaca984a
shlok <shloksomani@ymail.com>
2019-11-11 13:51:34 -0500

    added the routing in the biasNavbar and fixed the anti-patterns

---
+++ src/components/signup.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+
+export class Signup extends Component {
+	render() {
+		return (
+			<div class="row mt-5">
+				<div class="col-md-4 m-auto">
+					<div class="container-fluid bg-3 text-center">
+						<div
+							class="alert alert-warning alert-dismissible fade show"
+							role="alert"
+						></div>
+					</div>
+
+					<div class="card card-body">
+						<h1 class="text-center mb-3">
+							<i class="fas fa-user-plus"></i> Register
+						</h1>
+
+						<form action="/auth/signup" method="POST">
+							<div class="form-group">
+								<label for="name">Name</label>
+								<input
+									type="name"
+									id="name"
+									name="name"
+									class="form-control"
+									placeholder="Enter Name"
+									value="<%= typeof name != 'undefined' ? name : '' %>"
+									required
+								/>
+							</div>
+							<div class="form-group">
+								<label for="email">Email</label>
+								<input
+									type=""
+									id="email"
+									name="email"
+									class="form-control"
+									placeholder="Enter Email"
+									value="<%= typeof email != 'undefined' ? email : '' %>"
+									required
+								/>
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input
+									type="password"
+									id="password"
+									name="password"
+									class="form-control"
+									placeholder="Create Password"
+									value="<%= typeof password != 'undefined' ? password : '' %>"
+									required
+								/>
+							</div>
+							<div class="form-group">
+								<label for="password2">Confirm Password</label>
+								<input
+									type="password"
+									id="password2"
+									name="password2"
+									class="form-control"
+									placeholder="Confirm Password"
+									value="<%= typeof password2 != 'undefined' ? password2 : '' %>"
+									required
+								/>
+							</div>
+							<button type="submit" class="btn btn-primary btn-block">
+								Register
+							</button>
+						</form>
+						<p class="lead mt-4">
+							Have An Account? <a href="/auth/login">Login</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Signup;
